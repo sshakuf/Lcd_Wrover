15776478c08e7cdf2e934507f5f3c1e364700d96
Shahaf Shakuf <sshakuf@Shahafs-MacBook-Pro.local>
2020-08-01 19:07:57 +0300

    adding screen node

--- src/RestAPI.cpp
+++ src/RestAPI.cpp
@@ -23,11 +23,6 @@
 
 using namespace httpsserver;
 
-void RegisterEvents(HTTPServer *inServer)
-{
-    inServer->registerNode(new ResourceNode("/api/text/*", "GET", &handleTextEvent));
-}
-
 void handleTextEvent(HTTPRequest *req, HTTPResponse *res)
 {
     Serial.println("HandleTextEvent");
@@ -40,4 +35,32 @@ void handleTextEvent(HTTPRequest *req, HTTPResponse *res)
     // And return a successful response without body
     res->setStatusCode(204);
     res->setStatusText("No Content");
+}
+
+void HandleSetPinEvent(HTTPRequest *req, HTTPResponse *res)
+{
+    Serial.println("HandleSetPinEvent");
+    // Access the parameter from the URL. See Parameters example for more details on this
+    ResourceParameters *params = req->getParams();
+    size_t pin = std::atoi(params->getPathParameter(0).c_str());
+    size_t val = std::atoi(params->getPathParameter(1).c_str());
+
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, val);
+    String str = "Set pin " + String(pin) + " to " + String(val);
+    int nval = digitalRead(pin);
+    String str2 = " is: " + String(nval);
+
+    Serial.println(str + str2);
+    PutText("                        ", 0, 0, 2);
+    PutText(str, 0, 0, 2);
+    // And return a successful response without body
+    res->setStatusCode(204);
+    res->setStatusText("No Content");
+}
+
+void RegisterEvents(HTTPServer *inServer)
+{
+    inServer->registerNode(new ResourceNode("/api/text/*", "GET", &handleTextEvent));
+    inServer->registerNode(new ResourceNode("/api/pin/*/*", "GET", &HandleSetPinEvent));
 }
